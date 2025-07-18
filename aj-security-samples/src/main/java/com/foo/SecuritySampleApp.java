dfce86fccf1d2c321475c4782ccdbd364f0c347a
张鑫 <sp42@qq.com>
2025-07-18 17:51:55 +0800

    ci

--- aj-security-samples/src/main/java/com/foo/SecuritySampleApp.java
+++ aj-security-samples/src/main/java/com/foo/SecuritySampleApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FooApp {
+public class SecuritySampleApp {
     public static void main(String[] args) {
-        SpringApplication.run(FooApp.class, args);
+        SpringApplication.run(SecuritySampleApp.class, args);
     }
 }
